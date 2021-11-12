346224e17ead18a7208c09cb13a65a9e1fe00838
sreedevi-sree <sreedevifsd@gmail.com>
2021-11-12 16:39:47 +0530

    first commit

--- script.js
+++ script.js
@@ -1,18 +1,18 @@
-const dog_btn = document.getElementById("dog_btn");
-const dog_result = document.getElementById("dog_result");
-
-
-dog_btn.addEventListener("click", getRandomDog);
-
-
-function getRandomDog() {
-  fetch("https://random.dog/woof.json")
-    .then((res) => res.json())
-    .then((data) => {
-      if (data.url.includes(".mp4")) {
-        getRandomDog();
-      } else {
-        dog_result.innerHTML = `<img src=${data.url} width="300px" height="300px" alt="dog"/>`;
-      }
-    });
+document.getElementById("button1").addEventListener("click", getApi);
+function getApi() {
+  fetch("https://cdn.jsdelivr.net/gh/fawazahmed0/quran-api@1/editions.json").then((response) => {
+    response.json().then((data) => {
+      // console.log(data);
+      let result = "";
+      for(var x in data)
+{
+  result+=data[x].language+" ";  
+ 
 }
+console.log(result);
+     
+      document.getElementById("output").innerHTML = result;
+    });
+  });
+}
+
